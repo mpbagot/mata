51017fdee30c00ead24e3eba25a70be7fcbcc4ba
Mitchell Bagot <packof14eggies@gmail.com>
2017-10-05 13:47:48 +1100

    Add a Default Mods Module

---
+++ default_mods.py
@@ -0,0 +1,17 @@
+from mod import *
+
+class NNetAIMod(Mod):
+    def initialiseProperties(self):
+        '''
+        Initialise the name of the mod
+        '''
+        self.modName = 'NeuralNetAIMod'
+
+    def preLoad(self):
+        pass
+
+    def load(self):
+        pass
+
+    def postLoad(self):
+        pass
