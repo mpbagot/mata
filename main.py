6f4bf54ea77e3f46330acaf8487c257f2595e71c
Mitchell Bagot <packof14eggies@gmail.com>
2017-10-05 22:58:07 +1100

    Remove PacketHandler And Unused Import

--- main.py
+++ main.py
@@ -5,7 +5,6 @@
 - Integrated or Dedicated Server
 '''
 # Import the game submodules
-import network
 import mod
 import util
 import game
@@ -15,8 +14,6 @@ import sys
 
 # Initialise the ModLoader
 modLoader = mod.ModLoader()
-# TODO allow mods to add custom packet pipelines/handlers
-# packetHandler = network.PacketHandler()
 
 # Collect and handle the command line arguments
 arguments = sys.argv[1:]
