684aa86a5196308f781640bd3c35491bb771771a
Mitchell Bagot <packof14eggies@gmail.com>
2017-10-06 17:04:19 +1100

    Implement Connections And Packet Transmission

--- api/network.py
+++ api/network.py
@@ -4,16 +4,77 @@ A module for all packet handling between the server and client.
 '''
 # Import the game's modules
 import util
+from api.packets import ConfirmPacket, ByteSizePacket
 
 # Import the Python standard libraries
 import socket
+import io
+from threading import Thread
+from multiprocessing import Process
 
 class PacketHandler:
-    def __init__(self, side):
+    def __init__(self, game, side):
         self.nextSize = 37
+        self.game = game
         self.side = side
         self.connections = []
         self.safePackets = []
+
+        self.socket = socket.socket()
+
+        if side == util.SERVER:
+            self.socket.bind((socket.gethostname(), util.DEFAULT_PORT))
+            connPoll = Thread(target=self.pollForConnections)
+            connPoll.daemon = True
+            connPoll.start()
+
+    def connectToServer(self, address):
+        '''
+        A client-side method to connect to a chosen server
+        '''
+        self.socket.connect((address, util.DEFAULT_PORT))
+        self.connections.append(Connection(self.socket, address))
+        t = Thread(target=self.handleConn, args=(len(self.connections)-1,))
+        t.daemon = True
+        t.start()
+
+    def pollForConnections(self):
+        '''
+        A server-side method to poll for incoming connections from clients
+        '''
+        self.socket.listen(util.MAX_PLAYERS)
+        while True:
+            conn, addr = self.socket.accept()
+            self.connections.append(Connection(conn, addr))
+            t = Thread(target=self.handleConn, args=(len(self.connections)-1,))
+            t.daemon = True
+            t.start()
+
+    def handleConn(self, connIndex):
+        '''
+        Handle communication on the given connection
+        '''
+        conn = self.connections[connIndex].connObj
+        while True:
+            # Recieve the packet data
+            data = conn.recv(self.connections[connIndex].nextSize).decode()[1:-1]
+            dataDictionary = {a.split(':')[0][1:-1] : a.split(':')[1][1:-1] for a in data.split(',')}
+            # Loop through the registered packets and handle the recieved data accordingly
+            for packet in self.safePackets:
+                if packet.__name__ == dataDictionary['type']:
+                    # Initialise the packet, and handle it accordingly
+                    p = packet()
+                    p.fromBytes(dataDictionary['data'])
+                    # TODO get the game instance
+                    response = p.onRecieve(self.connections[connIndex], self.game)
+                    if packet.__name__ not in ['ConfirmPacket', 'ByteSizePacket'] and response is None:
+                        response = ConfirmPacket()
+                    if response:
+                        # If the packet is not a bytesize or confirmation packet,
+                        # then send a response and reset the receive size
+                        self.connections[connIndex].sendPacket(response)
+                        self.connections[connIndex].setNextPacketSize(37)
+                    break
 
     def registerPacket(self, packetClass):
         '''
@@ -26,12 +87,6 @@ class PacketHandler:
         Return if the packet is one of the registered packet types
         '''
         return any([isinstance(packet, a) for a in self.safePackets])
-
-    def setNextPacketSize(self, size):
-        '''
-        Set the size of the next packet to be recieved
-        '''
-        self.nextSize = size
 
     def sendToAll(self, packet):
         '''
@@ -58,10 +113,9 @@ class PacketHandler:
         if self.side == util.CLIENT:
             print('[WARNING] Cannot send a packet to clients from a client runtime!')
             return
-        # TODO send the packet
         for conn in self.connections:
             if conn.username == username:
-                pass
+                conn.sendPacket(packet)
 
     def sendToServer(self, packet):
         '''
@@ -74,28 +128,43 @@ class PacketHandler:
         if self.side == util.SERVER:
             print('[WARNING] Cannot send a packet to server from a server runtime!')
             return
-        # TODO send the packet
-        pass
+        self.connections[0].sendPacket(packet)
 
 class Connection:
-    def __init__(self):
+    def __init__(self, conn, addr):
         self.username = ''
+        self.connObj = conn
+        self.address = addr
+        self.nextSize = 37
 
-class Packet:
-    def toBytes(self, buf):
+    def sendPacket(self, packet):
         '''
-        Convert the data to a bytestring and write it to a buffer
+        Send a packet on this connection
         '''
-        raise NotImplementedError('toBytes method is empty in a packet class!')
+        # Prepare the packet buffer
+        buf = io.BytesIO()
+        buf.write('{"type":"{}","data":"'.format(packet.__class__.__name__).encode())
+        packet.toBytes(buf)
+        buf.write('"}'.encode())
 
-    def fromBytes(self, data):
-        '''
-        Read the data from the binary buffer, and convert it to a usable form
-        '''
-        raise NotImplementedError('fromBytes method is empty in a packet class!')
+        # Get the packet length to send first
+        byteSize = len(buf.getvalue())
 
-    def onRecieve(self, packetHandler, game):
+        # Initialise the ByteSizePacket and buffer
+        byteBuf = io.BytesIO()
+        sizePacket = ByteSizePacket(byteSize)
+
+        # Write the packet data to the buffer
+        byteBuf.write('{"type":"{}","data":"'.format(sizePacket.__class__.__name__).encode())
+        sizePacket.toBytes(byteBuf)
+        byteBuf.write('"}'.encode())
+
+        # Send the two packets one after the other
+        self.connObj.send(byteBuf.getvalue())
+        self.connObj.send(buf.getvalue())
+
+    def setNextPacketSize(self, size):
         '''
-        Run any required logic upon receiving the packet
+        Set the size of the next packet to be recieved
         '''
-        raise NotImplementedError('onRecieve method is empty in a packet class!')
+        self.nextSize = size
