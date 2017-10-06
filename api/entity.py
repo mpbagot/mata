dc70482fb87937e2968c28ede6a9aa1f77b856cf
Mitchell Bagot <packof14eggies@gmail.com>
2017-10-07 10:24:42 +1100

    Begin Filling Out Player Class

--- api/entity.py
+++ api/entity.py
@@ -1,6 +1,17 @@
+from api.item import Inventory
+
 class Player:
     def __init__(self):
-        self.username = 'admin'
+        self.username = ''
+        self.pos = [0, 0]
+        self.inventory = Inventory()
+        self.isDead = False
+
+    def setUsername(self, name):
+        '''
+        Set the player username to a given name
+        '''
+        self.username = name
 
     def toBytes(self):
         return b'player'
