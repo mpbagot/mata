da386e1b72d4f0810142b141476f54c62a78132c
Mitchell Bagot <packof14eggies@gmail.com>
2017-10-11 12:26:44 +1100

    Fill In Functions in Player Class

--- api/entity.pyx
+++ api/entity.pyx
@@ -4,6 +4,7 @@ class Player:
     def __init__(self):
         self.username = ''
         self.pos = [0, 0]
+        self.health = 100
         self.inventory = Inventory()
         self.isDead = False
         self.tickDamage = None
@@ -15,11 +16,20 @@ class Player:
         self.username = name
 
     def toBytes(self):
-        return b'player'
+        return (str([self.username, self.pos, self.health, str(self.tickDamage)]).replace(' ', '')).encode()
 
     @staticmethod
-    def fromBytes(self):
-        return Player()
+    def fromBytes(data):
+        uname, pos, health, damage = eval(data.decode())
+        p = Player()
+        p.username = uname
+        p.pos = pos
+        p.health = health
+        if damage:
+            p.tickDamage = Damage.fromBytes(damage.encode())
+        else:
+            p.tickDamage = None
+        return p
 
 class Entity:
     def __init__(self):
@@ -30,3 +40,10 @@ class Damage:
     def __init__(self, amount, source):
         self.amount = amount
         self.source = source
+
+    def __str__(self):
+        return ''
+
+    @staticmethod
+    def fromBytes(data):
+        return Damage(0, None)
