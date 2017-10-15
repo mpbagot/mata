325d7cb74d7db2e70850b99a718ddf0dcf55ad12
Mitchell Bagot <packof14eggies@gmail.com>
2017-10-15 14:13:17 +1100

    Fill In Some Packet Methods

--- api/packets.pyx
+++ api/packets.pyx
@@ -1,5 +1,8 @@
 from api.entity import Player
 from api.dimension import WorldMP
+from api.biome import TileMap
+
+import time
 
 class Packet:
     def toBytes(self, buf):
@@ -25,6 +28,7 @@ class ByteSizePacket(Packet):
         self.size = size
 
     def toBytes(self, buf):
+        print(self.size)
         buf.write(self.size.to_bytes(2, 'big'))
 
     def fromBytes(self, data):
@@ -53,37 +57,63 @@ class LoginPacket(Packet):
         return SyncPlayerPacketClient(self.player)
 
 class RequestWorldPacket(Packet):
-    def __init__(self, pos=None):
+    def __init__(self, dimId=None, pos=None):
         self.pos = pos
+        self.id = dimId
 
     def toBytes(self, buf):
         buf.write(str(self.pos).encode())
+        buf.write('|'.encode())
+        buf.write(self.id.to_bytes(2, 'big'))
 
     def fromBytes(self, data):
-        self.pos = eval(data.decode())
+        pos, id = data.split('|'.encode())
+        self.pos = eval(pos.decode())
+        self.id = int.from_bytes(id, 'big')
 
     def onReceive(self, connection, game):
-        return SendWorldPacket(game.world, self.pos)
+        worldData = game.modLoader.gameRegistry.dimensions[self.id].getWorldObj().generate(self.pos).world.map
+        tileMaps = [TileMap.createFromTiles(worldData[a:a+30]) for a in range(0, len(worldData), 30)]
+
+        return [SendWorldPacket(tileMap, self.pos, game, self.id, index+1, len(tileMaps)) for index, tileMap in enumerate(tileMaps)]
+
 
 class SendWorldPacket(Packet):
-    def __init__(self, world=None, pos=None):
-        self.world = world
+    def __init__(self, tiles=None, pos=None, game=None, dimId=None, part=1, length=None):
+        self.length = length
+        self.tiles = tiles
+        self.part = part
         self.pos = pos
+        self.id = dimId
+        self.game = game
 
     def toBytes(self, buf):
-        buf.write(self.world.generate(self.pos).toBytes(self.pos))
+        buf.write(self.length.to_bytes(1, 'big')+b'|')
+        buf.write(self.part.to_bytes(1, 'big'))
+        buf.write('|'.encode())
+        buf.write(self.id.to_bytes(1, 'big'))
+        buf.write('|'.encode())
+        # Get the biome list
+        biomes = self.game.modLoader.gameRegistry.dimensions[self.id].biomes
+        buf.write(self.tiles.toBytes(biomes))
 
     def fromBytes(self, data):
-        print(data)
-        self.world = WorldMP.fromBytes(data)
+        length, part, id, *tiles = data.split('|'.encode())
+        self.length = int.from_bytes(length, 'big')
+        self.part = int.from_bytes(part, 'big')
+        self.id = int.from_bytes(id, 'big')
+        self.tiles = '|'.encode().join(tiles)
 
     def onReceive(self, connection, game):
+        biomes = game.modLoader.gameRegistry.dimensions[self.id].biomes
+        start = time.time()
+        self.tiles = TileMap.fromBytes(self.tiles, biomes)
+        print('took', time.time()-start, 'to get world from bytes')
         if game.world is None:
             # Handle the screen change if necessary
-            game.openGui(game.getModInstance('ClientMod').gameGui, game)
-            game.world = self.world
-        else:
-            game.world.world = self.world.world
+            game.fireEvent('onGameLoad')
+            game.world = game.modLoader.gameRegistry.dimensions[self.id].getWorldObj()
+            game.world.world = self.tiles
         game.player.relPos = [0, 0]
 
 class SyncPlayerPacketClient(Packet):
@@ -102,7 +132,11 @@ class SyncPlayerPacketClient(Packet):
         game.player.pos = self.player.pos
         game.player.health = self.player.health
         print('player set by player sync packet')
-        return RequestWorldPacket(self.player.pos)
+        dimId = 0
+        for dim in game.modLoader.gameRegistry.dimensions:
+            if dim == game.player.dimension:
+                dimId = dim
+        return RequestWorldPacket(dimId, self.player.pos)
 
 class SyncPlayerPacketServer(Packet):
     def __init__(self, player=''):
@@ -115,9 +149,22 @@ class SyncPlayerPacketServer(Packet):
         self.player = Player.fromBytes(data)
 
     def onReceive(self, connection, game):
+        # Check if the player's motion is not greater than a certain threshold
+        # Update their position on the server if it is ok
+        # Reset them if it's not
+        playerList = game.modLoader.gameRegistry.dimensions[self.player.dimension].getWorldObj().players
+        serverPlayer = playerList[game.getPlayerIndex(self.player)]
+        if abs(self.player.pos[0]-serverPlayer.pos[0]) > 25 or abs(self.player.pos[1]-serverPlayer.pos[1]) > 25:
+            return SyncPlayerPacketClient(serverPlayer)
+        # If the player has clipped into a plant, reset their position
+        world = game.modLoader.gameRegistry.dimensions[self.player.dimension].getWorldObj()
+
+        # if world.world.map[self.player.pos[1]][self.player.pos[0]].plantIndex != -1:
+        #     return SyncPlayerPacketClient(serverPlayer)
+
         # Sync the player object on the server
-        playerIndex = game.world.getPlayerIndex(self.player.username)
-        game.world.players[playerIndex] = self.player
+        playerIndex = game.getPlayerIndex(self.player)
+        game.modLoader.gameRegistry.dimensions[self.player.dimension].getWorldObj().players[playerIndex] = self.player
 
 class DisconnectPacket(Packet):
     def __init__(self, message=''):
@@ -130,6 +177,4 @@ class DisconnectPacket(Packet):
         self.message = data.decode()
 
     def onReceive(self, connection, game):
-        # Open a GUI that displays the message, and disconnect them
-        game.openGui(game.getModInstance('ClientMod').disconnectMessageGui, self.message)
         del connection
