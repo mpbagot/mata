0bd1805c601939240b106ec7ff432a97be392610
Mitchell Bagot <packof14eggies@gmail.com>
2017-10-25 13:27:10 +1100

    Add Button For Character Maker Screen

--- mods/default/client/gui/extras.py
+++ mods/default/client/gui/extras.py
@@ -1,4 +1,22 @@
 from api.gui import *
+from mods.default.packets import *
+
+class StartGameButton(Button):
+    def __init__(self, rect):
+        super().__init__(rect, 'Play!')
+
+    def onClick(self, game):
+        # Generate the image values array
+        values = game.openGUI[1].valSliders
+        playerImg = [slider.value for slider in values]
+
+        # Set it and sync it to the server
+        game.player.img = playerImg
+        packetPipeline = game.getModInstance('ClientMod').packetPipeline
+        packetPipeline.sendToServer(SendPlayerImagePacket(game.player))
+
+        # Show the game screen
+        game.openGui(game.getModInstance('ClientMod').gameGui, game)
 
 class PlayButton(Button):
     def __init__(self, rect):
@@ -8,11 +26,14 @@ class PlayButton(Button):
         # Grab the variables from the textboxes
         username = game.openGUI[1].textboxes[0].text
         address = game.openGUI[1].textboxes[1].text
+
         # Set the player username
         game.player.setUsername(username)
         game.openGui(game.getModInstance('ClientMod').loadingGui)
+
         # Try to connect
         error = game.getModInstance('ClientMod').packetPipeline.connectToServer(address)
+
         # Display an error if it fails for any reason
         if error:
             game.openGui(game.getModInstance('ClientMod').mainMenuGui)
