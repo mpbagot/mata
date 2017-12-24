f5321632f90eabfd0034c06ac2cf3478777e9619
Mitchell Bagot <packof14eggies@gmail.com>
2017-12-24 15:12:37 +1100

    Add Setup Scripts To Install Dependencies

---
+++ setup.bat
@@ -0,0 +1,4 @@
+py -m pip install noise
+py -m pip install pygame
+CALL pythonify_api.bat
+echo "Setup Complete, Run main.py to launch game..."
---
+++ setup.sh
@@ -0,0 +1,4 @@
+pip3 install pygame
+pip3 install noise
+./pythonify_api.sh
+echo "Setup Complete, Run main.py to launch game..."
