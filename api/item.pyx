c50404c7e3d7f460df40f0c3ecc2b553c03191a0
Mitchell Bagot <packof14eggies@gmail.com>
2018-01-03 18:16:57 +1100

    Add Methods To Inventory Class

--- api/item.pyx
+++ api/item.pyx
@@ -17,17 +17,56 @@ class Inventory:
     def toBytes(self):
         return b''
 
+    def hashInv(self):
+        return ''
+
+    def sortItems(self):
+        self.sortGroup('hotbar')
+        self.sortGroup('main')
+
+    def sortGroup(self, name):
+        used = []
+        for i, itemstack in self.items[name]:
+            if i in used:
+                continue
+            for j, item in self.items[name]:
+                if j != i and item.getItemName() == itemstack.getItemName():
+                    used.append(j)
+                    result, carryover = itemstack.add(item)
+                    self.items[name][i] = result
+                    self.items[name][j] = carryover
+
+    @staticmethod
+    def addInventory(inv1, inv2):
+        '''
+        Return an inventory that is the sum of two inventories
+        '''
+        sumInv = Inventory()
+        sumInv.items = inv1.items
+
+        sumInv.items['main'] += inv2.items['hotbar']
+        sumInv.items['main'] += inv2.items['main']
+        sumInv.items['main'] += [inv2.items['left'], inv2.items['right']]
+
+        sumInv.sortItems()
+
+        return sumInv
+
 class Item:
     def __init__(self, resources):
+        self.name = ''
         try:
             self.img = resources['item_'+self.getItemName()]
         except KeyError:
             import pygame
-            self.img = pygame.image.load('resources/textures/mods/tiles/dirt.png')
+            self.img = pygame.image.load('resources/textures/mods/tiles/dirt.png')#.convert_alpha()
             #raise Exception('Item Image has not been Registered!')
 
     def getItemName(self):
-        return 'null_item'
+        return self.name or 'null_item'
+
+    def getMaxStackSize(self):
+        return 1
 
     def hasAttribute(self, name):
         '''
@@ -43,3 +82,12 @@ class ItemStack:
     def __init__(self, item, size):
         self.stackSize = size
         self.item = item
+
+    def getItem(self):
+        return self.item
+
+    def getMaxStackSize(self):
+        return self.item.getMaxStackSize()
+
+    def add(self, other):
+        return [self, other]
