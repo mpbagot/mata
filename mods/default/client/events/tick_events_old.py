f5eabea02a459013b97d5423ad9027acd9036b9d
Mitchell Bagot <packof14eggies@gmail.com>
2018-01-25 10:03:17 +1100

    Push world generation into a new Process, rather than Thread

--- mods/default/client/events/tick_events_old.py
+++ mods/default/client/events/tick_events_old.py
@@ -1,8 +1,6 @@
 from api.packets import *
-from api.properties import *
 
 from threading import Thread
-from multiprocessing import Process, Queue
 import pygame
 from copy import deepcopy
 
@@ -14,22 +12,15 @@ def onTickGenerateWorld(game, tick):
     if game.openGUI[0] == game.getModInstance('ClientMod').gameGui:
         # If the player has moved more than a certain distance, generate the world
         absRelPos = [abs(a) for a in game.player.relPos]
-        # TODO this still doesn't work
-        if (game.player.synced and not game.world.isWorldLoaded()) or max(absRelPos) > 16:
+        if (game.player.synced and not game.world.world) or max(absRelPos) > 26:
             # Set the second relative position to start iterating
             props = game.player.getProperty('relPos2')
 
-            if props.props['ready']:
-                return
-
             # Generate the world on the client
-            queue = Queue()
-            p = Process(target=genWorld, args=(game, props, queue))
-            t = Thread(target=handleProcess, args=(game, queue))
-            p.daemon = True
+            t = Thread(target=genWorld, args=(game, props))
             t.daemon = True
-            p.start()
             t.start()
+
 
 def onTickHandleMovement(game, tick):
     '''
@@ -116,61 +107,34 @@ def onTickSyncPlayer(game, tick):
                 # Send the copy of the player object in the packet
                 game.getModInstance('ClientMod').packetPipeline.sendToServer(SyncPlayerPacket(playerCopy))
 
-def handleProcess(game, queue):
-    '''
-    Handle the genWorld queue and link it back into the main thread
-    '''
-    while True:
-        data = queue.get()
-        # If it's a player property, update the player
-        if isinstance(data, Property):
-            game.player.setProperty('relPos2', data)
-        # If it's a player, update the whole player
-        elif isinstance(data, Player):
-            game.player = data
-        # If it's a tilemap, update that
-        elif data != "end":
-            for y, row in enumerate(data.map):
-                for x, tile in enumerate(row):
-                    data.map[y][x].resetTile(game.modLoader.gameRegistry.resources)#noiseMap[y][x], 0, game.modLoader.gameRegistry.resources)
-
-            game.world.setTileMap(data)
-            queue.put(game.player.getProperty('relPos2'))
-
-        # Otherwise, end the thread
-        else:
-            return
-
-def genWorld(game, props, queue):
+def genWorld(game, props):
     '''
     Generate the small area of the world
     '''
+    if props.props['ready'] == True:
+        return
     # Set the abs pos of the player
     preGenPos = game.player.getAbsPos()
     print('genning world')
     props.props['ready'] = True
-    queue.put(props)
+    game.player.setProperty('relPos2', props)
 
     # Generate the world
-    dimension = game.getDimension(game.player.dimension)
-    dimension.generate(preGenPos, game.modLoader.gameRegistry)
-    # Fix the pygame surface breaking when sent through the Queue
-    queue.put(dimension.getWorldObj().getTileMap())
+    world = game.getWorld(game.player.dimension)#game.modLoader.gameRegistry.dimensions[game.player.dimension].getWorldObj()
+    worldData = world.generate(preGenPos, game.modLoader.gameRegistry).world.map
+    game.world.world.map = worldData
     print('world gen done')
 
     # Fetch the player property
-    props = queue.get()
-    #props = game.player.getProperty('relPos2')
+    props = game.player.getProperty('relPos2')
 
     # Move the player
     game.player.pos = preGenPos
     game.player.relPos = list(props.props['pos'])
-    queue.put(game.player)
 
     # Update the player property
     props.props['ready'] = False
     props.props['pos'] = [0, 0]
 
     # Set the property back into the player
-    queue.put(props)
-    queue.put('end')
+    game.player.setProperty('relPos2', props)
