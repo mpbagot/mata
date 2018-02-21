64902673ddfeb4acca8e6173692b8ed4fe3f0f88
Mitchell Bagot <packof14eggies@gmail.com>
2018-02-21 14:10:47 +1100

    Move command handling code back into api

--- api/cmd.pyx
+++ api/cmd.pyx
@@ -11,10 +11,24 @@ class Command:
 
 class FailedCommand(Command):
     def run(self, username, *args):
-        # Let onCommand hooks handle it
+        # Send a failure message back to the client
+        self.game.packetPipeline.sendToPlayer(SendCommandPacket('/message global '+args[0]+' is not a valid command.'), username)
         return
 
 class MessageCommand(Command):
     def run(self, username, *args):
-        # Let onCommand hooks handle it
-        return
+        # Redirect the message if on the server
+        if self.game.args.getRuntimeType() == util.SERVER:
+          pp = self.game.packetPipeline
+          # Send messages back to one or more clients based on the message mode
+          mode = args[0]
+          args = [args[0], '<{}>'.format(username)]+list(args[1:])
+          if mode == 'global':
+              pp.sendToAll(SendCommandPacket('/message '+' '.join(args)))
+          elif mode == 'local':
+              pp.sendToNearby(SendCommandPacket('/message '+' '.join(args)), username)
+          else:
+              # Send /message <username> message here to the user
+              # Send /message <user> message here to username
+              pp.sendToPlayer(SendCommandPacket('/message '+' '.join(args)), username)
+              pp.sendToPlayer(SendCommandPacket('/message {} {}'.format(username, ' '.join(args[1:]))), args[0])
