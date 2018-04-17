9e4824f32c9503c0559cf4ae004240cf136c0cb0
Mitchell Bagot <packof14eggies@gmail.com>
2018-04-17 17:21:09 +1000

    Add Weapon class to API

---
+++ api/combat.pyx
@@ -0,0 +1,27 @@
+# Weapon Damage Classes [0-20]
+NORMAL = 0
+FIRE = 1
+ICE = 2
+LIFE = 3
+UNDEAD = 4
+MAGIC = 5
+
+# Weapon Range Constants
+MELEE = 1
+BIG_MELEE = 3
+SHORT_RANGED = 10
+
+INFINITE = 0 # 0 means no limits
+
+# Weapon Spread constants
+THIN_ARC = 0.1
+MID_ARC = 0.25
+WIDE_ARC = 0.33
+SEMI_ARC = 0.5
+FULL_ARC = 1
+
+# Weapon knockback constants
+KNOCK_NONE = 0
+KNOCK_WEAK = 1
+KNOCK_STRONG = 5
+KNOCK_INFINITE = 65000
--- api/item.pyx
+++ api/item.pyx
@@ -2,6 +2,8 @@
 item.py
 A module to hold all the api stuff related to items
 '''
+from api import combat
+from api.entity import *
 
 class Inventory:
     def __init__(self):
@@ -31,12 +33,6 @@ class Inventory:
         # Pull the different sections of the packet from the data
         armour, left, right = [ItemStack.fromBytes(game, bytes[a:a+98]) for a in range(0, 294, 98)]
         bytes = bytes[294:]
-
-        # left = ItemStack.fromBytes(game, bytes[:98])
-        # bytes = bytes[98:]
-        #
-        # right = ItemStack.fromBytes(game, bytes[:98])
-        # bytes = bytes[98:]
 
         hotbar = [ItemStack.fromBytes(game, bytes[a*98:(a+1)*98]) for a in range(10)]
         bytes = bytes[980:]
@@ -141,6 +137,33 @@ class Inventory:
         # Sort by item name
         self.items[name] = sorted([a for a in newGroup if a])
 
+    def checkWeapon(self, weapon):
+        '''
+        Check if given weapon is equipped
+        '''
+        return weapon == self.items['left'].getItem() or weapon == self.items['right'].getItem()
+
+    def addItemstack(self, itemstack):
+        '''
+        Add an itemstack to the main section of the inventory
+        '''
+        if len(self.items['main']) < self.maxSize:
+            self.items['main'].append(itemstack)
+
+        else:
+            for i, stack in enumerate(self.items['main']):
+                if stack.getRegistryName() == itemstack.getRegistryName():
+                    result, carry = stack.add(itemstack)
+                    if not carry:
+                        self.items['main'][i] = result
+                        return
+
+                if stack.getRegistryName() == 'null_item':
+                    self.items['main'][i] = itemstack
+                    return
+
+        print('[ERROR] New Itemstack cannot fit')
+
     @staticmethod
     def addInventory(inv1, inv2, force=False):
         '''
@@ -173,61 +196,6 @@ class Inventory:
         overflowInv.sortItems()
 
         return [sumInv, overflowInv]
-
-class Item:
-    def __init__(self, resources):
-        self.name = ''
-        self.image = None
-
-    def setRegistryName(self, name):
-        self.name = name
-
-    def getRegistryName(self):
-        return self.name
-
-    def getMaxStackSize(self):
-        return 1
-
-    def hasAttribute(self, name):
-        '''
-        Return whether the class (and classes which extend this) has a given attribute
-        '''
-        try:
-            a = self.__getattribute__(name)
-            return True
-        except AttributeError:
-            return False
-
-    def getImage(self, resources):
-        try:
-            return resources.get(self.image)
-        except KeyError:
-            raise KeyError('Image "{}" has not been registered in the Game Registry'.format(self.image))
-
-    def toBytes(self):
-        '''
-        Get a byte representation of the item
-        '''
-        className = self.__class__.__name__
-        className = ' '*(32-len(className))+className
-        image = self.image if isinstance(self.image, str) else ''
-        image = ' '*(32-len(image))+image
-        itemName = ' '*(32-len(self.name))+self.name
-        return itemName.encode()+image.encode()+className.encode()
-
-    def fromBytes(itemClasses, resources, data):
-        itemName, image, className = [data[a:a+32].decode().strip() for a in range(0, len(data), 32)]
-
-        # Iterate the possible classes and look for the matching item class
-        for itemClass in itemClasses:
-            if itemClass.__name__ == className:
-                # Fill in the values for the item, then return it
-                item = itemClass(resources)
-                item.name = itemName
-                item.image = image
-                return item
-
-        return NullItem()
 
 class ItemStack:
     def __init__(self, item, size):
@@ -287,7 +255,7 @@ class ItemStack:
         sumSize = self.stackSize + other.stackSize
         carrySize = 0
         if not forceCompress and sumSize > self.getMaxStackSize():
-            carrySize = sumSize%self.getMaxStackSize()
+            carrySize = sumSize-self.getMaxStackSize()
             sumSize = self.getMaxStackSize()
 
         # Create the stacks accordingly
@@ -297,6 +265,133 @@ class ItemStack:
             carryover = ItemStack(self.getItem(), carrySize)
 
         return [result, carryover]
+
+class Item:
+    def __init__(self, resources):
+        self.name = ''
+        self.image = None
+
+    def __eq__(self, other):
+        if not isinstance(other, Item):
+            return False
+        return self.name == other.name
+
+    def setRegistryName(self, name):
+        self.name = name
+
+    def getRegistryName(self):
+        return self.name
+
+    def getMaxStackSize(self):
+        return 1
+
+    def hasAttribute(self, name):
+        '''
+        Return whether the class (and classes which extend this) has a given attribute
+        '''
+        try:
+            a = self.__getattribute__(name)
+            return True
+        except AttributeError:
+            return False
+
+    def getImage(self, resources):
+        try:
+            return resources.get(self.image)
+        except KeyError:
+            raise KeyError('Image "{}" has not been registered in the Game Registry'.format(self.image))
+
+    def toBytes(self):
+        '''
+        Get a byte representation of the item
+        '''
+        className = self.__class__.__name__
+        className = ' '*(32-len(className))+className
+        image = self.image if isinstance(self.image, str) else ''
+        image = ' '*(32-len(image))+image
+        itemName = ' '*(32-len(self.name))+self.name
+        return itemName.encode()+image.encode()+className.encode()
+
+    def fromBytes(itemClasses, resources, data):
+        itemName, image, className = [data[a:a+32].decode().strip() for a in range(0, len(data), 32)]
+
+        # Iterate the possible classes and look for the matching item class
+        for itemClass in itemClasses:
+            if itemClass.__name__ == className:
+                # Fill in the values for the item, then return it
+                item = itemClass(resources)
+                item.name = itemName
+                item.image = image
+                return item
+
+        return NullItem()
+
+class Weapon(Item):
+    def __init__(self, resources):
+        super().__init__(resources)
+        self.attack = 0
+        self.damageClass = combat.NORMAL
+        self.range = combat.MELEE
+        self.knockback = combat.KNOCK_NONE
+        self.spread = combat.WIDE_ARC
+
+    def __eq__(self, other):
+        if not isinstance(other, Weapon):
+            return False
+        varComparisons = [
+                          self.attack == other.attack,
+                          self.range == other.range,
+                          self.knockback == other.knockback,
+                          self.damageClass == other.damageClass,
+                          self.spread == other.spread
+                         ]
+        return super().__eq__(other) and all(varComparisons)
+
+    def toBytes(self):
+        # Get the default item bytes
+        itemData = super().toBytes()
+        # Then append the weapon-specific information to it
+        itemData += self.attack.to_bytes(3, 'big') + self.range.to_bytes(4, 'big')
+        itemData += self.damageClass.to_bytes(1, 'big')
+        itemData += self.knockback.to_bytes(2, 'big')
+        return itemData
+
+    def fromBytes(itemClasses, resources, data):
+        itemName, image, className = [data[a:a+32].decode().strip() for a in range(0, 96, 32)]
+        data = data[96:]
+
+        attack = int.from_bytes(data[:3], 'big')
+        attackRange = int.from_bytes(data[3:7], 'big')
+        damageClass = data[7]
+        knockback = int.from_bytes(data[8:], 'big')
+
+        # Iterate the possible classes and look for the matching item class
+        for itemClass in itemClasses:
+            if itemClass.__name__ == className:
+                # Fill in the values for the item, then return it
+                item = itemClass(resources)
+                item.name = itemName
+                item.image = image
+                item.attack = attack
+                item.range = attackRange
+                item.damageClass = damageClass
+                item.knockback = knockback
+                return item
+
+        return NullItem()
+
+    def calcDamage(self, game, source, entityList):
+        '''
+        Calculate the damage and call event hooks for this weapon
+        against a list of entities
+        '''
+        for entity in entityList:
+            damage = self.attack
+            # TODO Apply Damage multipliers based on type
+
+            # TODO Allow event hooks to modify the damage
+
+            game.getEntity(entity.uuid).tickDamage = Damage(damage, source)
 
 class NullItem(Item):
     '''
